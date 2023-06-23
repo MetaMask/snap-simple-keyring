776c69d13fd4d914b45430aabc97405f5df67b21
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-06-23 15:52:31 +0200

    fix: hard-code snap origin for now

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,5 +2,4 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-export const defaultSnapOrigin =
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+export const defaultSnapOrigin = 'npm:@metamask/snap-simple-keyring-snap';
