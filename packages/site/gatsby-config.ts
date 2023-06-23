480871fbfad179fb071e62301f96afc8a6ac4496
Daniel Rocha <daniel.rocha@consensys.net>
2023-06-23 15:06:40 +0200

    ci: define path prefix when building site (#30)

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -5,6 +5,8 @@ const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: 'automatic',
 
+  pathPrefix: process.env.GATSBY_PATH_PREFIX ?? '/',
+
   plugins: [
     'gatsby-plugin-svgr',
     'gatsby-plugin-styled-components',
