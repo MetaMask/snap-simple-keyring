14827f58e081436145c55c399f4110959b640ab3
Owen Craston <owengc12@gmail.com>
2023-08-22 13:46:56 -0700

    reset defaultSnapOrigin

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,4 +2,4 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-export const defaultSnapOrigin = 'local:http://localhost:8080';
+export const defaultSnapOrigin = 'npm:@metamask/snap-simple-keyring-snap';
