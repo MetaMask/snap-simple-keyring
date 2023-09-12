89e24444991d0b105dcd4995836c82acdab0f697
Daniel Rocha <daniel.rocha@consensys.net>
2023-09-12 17:45:12 +0200

    feat: migrate to new `keyring-api` version (`0.2.x`) (#64)
    * feat: migrate snap to API 0.2.2
    
    * chore: run `yarn dedupe`
    
    * chore: remove old comment
    
    * chore: use sync flow by default
    
    * chore: update UI and `personal_sign` to match new API

--- packages/snap/src/keyring.ts
+++ packages/snap/src/keyring.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 import { Common, Hardfork } from '@ethereumjs/common';
-import { JsonTx, TransactionFactory } from '@ethereumjs/tx';
+import { TransactionFactory } from '@ethereumjs/tx';
 import {
   Address,
   ecsign,
@@ -20,28 +20,26 @@ import {
   signTypedData,
 } from '@metamask/eth-sig-util';
 import {
+  EthAccountType,
+  EthMethod,
   Keyring,
   KeyringAccount,
   KeyringRequest,
   SubmitRequestResponse,
+  emitSnapKeyringEvent,
 } from '@metamask/keyring-api';
-import type { Json, JsonRpcRequest } from '@metamask/utils';
+import { KeyringEvent } from '@metamask/keyring-api/dist/events';
+import { type Json, type JsonRpcRequest } from '@metamask/utils';
 import { Buffer } from 'buffer';
 import { v4 as uuid } from 'uuid';
 
-import { SigningMethods } from './permissions';
 import { saveState } from './stateManagement';
-import {
-  isEvmChain,
-  serializeTransaction,
-  isUniqueAccountName,
-  isUniqueAddress,
-} from './util';
+import { isEvmChain, serializeTransaction, isUniqueAddress } from './util';
 
 export type KeyringState = {
   wallets: Record<string, Wallet>;
   requests: Record<string, KeyringRequest>;
-  useSynchronousApprovals: boolean;
+  useSyncApprovals: boolean;
 };
 
 export type Wallet = {
@@ -52,14 +50,14 @@ export type Wallet = {
 export class SimpleKeyring implements Keyring {
   #wallets: Record<string, Wallet>;
 
-  #useSynchronousApprovals = false;
+  #useSyncApprovals = true;
 
   #pendingRequests: Record<string, KeyringRequest>;
 
   constructor(state: KeyringState) {
     this.#wallets = state.wallets;
     this.#pendingRequests = state.requests;
-    this.#useSynchronousApprovals = state.useSynchronousApprovals || false;
+    this.#useSyncApprovals = state.useSyncApprovals || false;
   }
 
   async listAccounts(): Promise<KeyringAccount[]> {
@@ -71,16 +69,11 @@ export class SimpleKeyring implements Keyring {
   }
 
   async createAccount(
-    name: string,
-    options: Record<string, Json> | null = null,
+    options: Record<string, Json> = {},
   ): Promise<KeyringAccount> {
     const { privateKey, address } = this.#getKeyPair(
       options?.privateKey as string | undefined,
     );
-
-    if (!isUniqueAccountName(name, Object.values(this.#wallets))) {
-      throw new Error(`Account name already in use: ${name}`);
-    }
 
     if (!isUniqueAddress(address, Object.values(this.#wallets))) {
       throw new Error(`Account address already in use: ${address}`);
@@ -88,42 +81,25 @@ export class SimpleKeyring implements Keyring {
 
     const account: KeyringAccount = {
       id: uuid(),
-      name,
       options,
       address,
-      supportedMethods: [
-        'eth_sendTransaction',
-        'eth_sign',
-        'eth_signTransaction',
-        'eth_signTypedData_v1',
-        'eth_signTypedData_v2',
-        'eth_signTypedData_v3',
-        'eth_signTypedData_v4',
-        'eth_signTypedData',
-        'personal_sign',
+      methods: [
+        EthMethod.PersonalSign,
+        EthMethod.Sign,
+        EthMethod.SignTransaction,
+        EthMethod.SignTypedData,
+        EthMethod.SignTypedDataV1,
+        EthMethod.SignTypedDataV3,
+        EthMethod.SignTypedDataV4,
       ],
-      type: 'eip155:eoa',
+      type: EthAccountType.Eoa,
     };
 
     this.#wallets[account.id] = { account, privateKey };
     await this.#saveState();
+    await this.#emitEvent(KeyringEvent.AccountCreated, { account });
 
-    await snap.request({
-      method: 'snap_manageAccounts',
-      params: {
-        method: 'createAccount',
-        params: { account },
-      },
-    });
     return account;
-  }
-
-  toggleSynchronousApprovals(): void {
-    this.#useSynchronousApprovals = !this.#useSynchronousApprovals;
-  }
-
-  isSynchronousMode(): boolean {
-    return this.#useSynchronousApprovals;
   }
 
   async filterAccountChains(_id: string, chains: string[]): Promise<string[]> {
@@ -139,38 +115,20 @@ export class SimpleKeyring implements Keyring {
       ...account,
       // Restore read-only properties.
       address: currentAccount.address,
-      supportedMethods: currentAccount.supportedMethods,
+      methods: currentAccount.methods,
       type: currentAccount.type,
       options: currentAccount.options,
     };
 
-    if (!isUniqueAccountName(account.name, Object.values(this.#wallets))) {
-      throw new Error(`Account name already in use: ${account.name}`);
-    }
-
     this.#wallets[account.id].account = newAccount;
     await this.#saveState();
-
-    await snap.request({
-      method: 'snap_manageAccounts',
-      params: {
-        method: 'updateAccount',
-        params: { account },
-      },
-    });
+    await this.#emitEvent(KeyringEvent.AccountUpdated, { account });
   }
 
   async deleteAccount(id: string): Promise<void> {
     delete this.#wallets[id];
     await this.#saveState();
-
-    await snap.request({
-      method: 'snap_manageAccounts',
-      params: {
-        method: 'deleteAccount',
-        params: { id },
-      },
-    });
+    await this.#emitEvent(KeyringEvent.AccountDeleted, { id });
   }
 
   async listRequests(): Promise<KeyringRequest[]> {
@@ -179,108 +137,61 @@ export class SimpleKeyring implements Keyring {
 
   async getRequest(id: string): Promise<KeyringRequest> {
     if (this.#pendingRequests[id] === undefined) {
-      return this.#pendingRequests[id];
+      throw new Error(`No pending request found with id: ${id}`);
     }
-    throw new Error(`No pending request found with id: ${id}`);
+
+    return this.#pendingRequests[id];
   }
 
-  // This snap implements a synchronous keyring, which means that the request
-  // doesn't need to be approved and the execution result will be returned to
-  // the caller by the `submitRequest` method.
-  //
-  // In an asynchronous implementation, the request should be stored in queue
-  // of pending requests to be approved or rejected by the user.
   async submitRequest(request: KeyringRequest): Promise<SubmitRequestResponse> {
-    if (this.#useSynchronousApprovals) {
-      return this.#handleSynchronousSubmitRequest(request);
-    }
-    return this.#handleAsyncSubmitRequest(request);
+    return this.#useSyncApprovals
+      ? this.#syncSubmitRequest(request)
+      : this.#asyncSubmitRequest(request);
   }
 
   async approveRequest(id: string): Promise<void> {
-    if (this.#useSynchronousApprovals) {
-      throw new Error(
-        'The "approveRequest" method is not available when synchronous approvals are enabled. Disable synchronous approvals by calling toggleSynchronousApprovals.',
-      );
-    } else {
-      try {
-        const request: KeyringRequest = await this.getRequest(id);
-        const { method, params = '' } = request.request as JsonRpcRequest;
-        const signature = this.#handleSigningRequest(method, params);
-        await this.#removePendingRequest(id);
-        await snap.request({
-          method: 'snap_manageAccounts',
-          params: {
-            method: 'submitResponse',
-            params: { id, result: signature },
-          },
-        });
-      } catch (error) {
-        throw new Error(
-          `Cannot approve request with id: ${id}. Error: ${
-            (error as Error).message
-          }`,
-        );
-      }
+    if (this.#pendingRequests[id] === undefined) {
+      throw new Error(`No pending request found with id: ${id}`);
     }
+
+    const { request } = await this.getRequest(id);
+    const result = this.#handleSigningRequest(
+      request.method,
+      request.params ?? [],
+    );
+    await this.#removePendingRequest(id);
+    await this.#emitEvent(KeyringEvent.RequestApproved, { id, result });
   }
 
   async rejectRequest(id: string): Promise<void> {
-    if (this.#useSynchronousApprovals) {
-      throw new Error(
-        'The "rejectRequest" method is not available when synchronous approvals are enabled. Disable synchronous approvals by calling toggleSynchronousApprovals.',
-      );
-    } else {
-      try {
-        // Check if id is in pendingRequests list
-        if (this.#pendingRequests[id] === undefined) {
-          throw new Error(`No pending request found with id: ${id}`);
-        }
-        await this.#removePendingRequest(id);
-        await snap.request({
-          method: 'snap_manageAccounts',
-          params: {
-            method: 'submitResponse',
-            params: { id, result: null },
-          },
-        });
-      } catch (error) {
-        throw new Error(
-          `Cannot reject request with id: ${id}. Error: ${
-            (error as Error).message
-          }`,
-        );
-      }
+    if (this.#pendingRequests[id] === undefined) {
+      throw new Error(`No pending request found with id: ${id}`);
     }
+
+    await this.#removePendingRequest(id);
+    await this.#emitEvent(KeyringEvent.RequestRejected, { id });
   }
 
   async #removePendingRequest(id: string): Promise<void> {
-    try {
-      delete this.#pendingRequests[id];
-      await this.#saveState();
-    } catch (error) {
-      throw new Error(
-        `Cannot remove pending request with id: ${id}. Error: ${
-          (error as Error).message
-        }`,
-      );
-    }
+    delete this.#pendingRequests[id];
+    await this.#saveState();
   }
 
-  async #handleAsyncSubmitRequest(
+  async #asyncSubmitRequest(
     request: KeyringRequest,
   ): Promise<SubmitRequestResponse> {
-    this.#pendingRequests[request.request.id] = request;
+    this.#pendingRequests[request.id] = request;
     await this.#saveState();
     return {
       pending: true,
+      redirect: {},
     };
   }
 
-  async #handleSynchronousSubmitRequest(
+  async #syncSubmitRequest(
     request: KeyringRequest,
   ): Promise<SubmitRequestResponse> {
-    const { method, params = '' } = request.request as JsonRpcRequest;
+    const { method, params = [] } = request.request as JsonRpcRequest;
     const signature = this.#handleSigningRequest(method, params);
     return {
       pending: false,
@@ -289,15 +200,15 @@ export class SimpleKeyring implements Keyring {
   }
 
   #getWalletByAddress(address: string): Wallet {
-    const walletMatch = Object.values(this.#wallets).find(
+    const match = Object.values(this.#wallets).find(
       (wallet) =>
         wallet.account.address.toLowerCase() === address.toLowerCase(),
     );
 
-    if (walletMatch === undefined) {
-      throw new Error(`Cannot find wallet for address: ${address}`);
+    if (match === undefined) {
+      throw new Error(`Account '${address}' not found`);
     }
-    return walletMatch;
+    return match;
   }
 
   #getKeyPair(privateKey?: string): {
@@ -320,32 +231,39 @@ export class SimpleKeyring implements Keyring {
 
   #handleSigningRequest(method: string, params: Json): Json {
     switch (method) {
-      case 'personal_sign': {
-        const [from, message] = params as string[];
+      case EthMethod.PersonalSign: {
+        const [message, from] = params as [string, string];
         return this.#signPersonalMessage(from, message);
       }
 
-      case 'eth_sendTransaction':
-      case 'eth_signTransaction':
-      case SigningMethods.SignTransaction: {
-        const [from, tx, opts] = params as [string, JsonTx, Json];
-        return this.#signTransaction(from, tx, opts);
+      case EthMethod.SignTransaction: {
+        const [tx] = params as [any];
+        return this.#signTransaction(tx);
       }
 
-      case 'eth_signTypedData':
-      case 'eth_signTypedData_v1':
-      case 'eth_signTypedData_v2':
-      case 'eth_signTypedData_v3':
-      case 'eth_signTypedData_v4': {
-        const [from, data, opts] = params as [
-          string,
-          Json,
-          { version: SignTypedDataVersion },
-        ];
-        return this.#signTypedData(from, data, opts);
+      case EthMethod.SignTypedData:
+      case EthMethod.SignTypedDataV1: {
+        const [from, data] = params as [string, Json];
+        return this.#signTypedData(from, data, {
+          version: SignTypedDataVersion.V1,
+        });
       }
 
-      case 'eth_sign': {
+      case EthMethod.SignTypedDataV3: {
+        const [from, data] = params as [string, Json];
+        return this.#signTypedData(from, data, {
+          version: SignTypedDataVersion.V3,
+        });
+      }
+
+      case EthMethod.SignTypedDataV4: {
+        const [from, data] = params as [string, Json];
+        return this.#signTypedData(from, data, {
+          version: SignTypedDataVersion.V4,
+        });
+      }
+
+      case EthMethod.Sign: {
         const [from, data] = params as [string, string];
         return this.#signMessage(from, data);
       }
@@ -356,13 +274,13 @@ export class SimpleKeyring implements Keyring {
     }
   }
 
-  #signTransaction(from: string, tx: any, _opts: any): Json {
+  #signTransaction(tx: any): Json {
     // Patch the transaction to make sure that the `chainId` is a hex string.
     if (!tx.chainId.startsWith('0x')) {
       tx.chainId = `0x${parseInt(tx.chainId, 10).toString(16)}`;
     }
 
-    const wallet = this.#getWalletByAddress(from);
+    const wallet = this.#getWalletByAddress(tx.from);
     const privateKey = Buffer.from(wallet.privateKey, 'hex');
     const common = Common.custom(
       { chainId: tx.chainId },
@@ -433,7 +351,23 @@ export class SimpleKeyring implements Keyring {
     await saveState({
       wallets: this.#wallets,
       requests: this.#pendingRequests,
-      useSynchronousApprovals: this.#useSynchronousApprovals,
+      useSyncApprovals: this.#useSyncApprovals,
     });
+  }
+
+  async #emitEvent(
+    event: KeyringEvent,
+    data: Record<string, Json>,
+  ): Promise<void> {
+    await emitSnapKeyringEvent(snap, event, data);
+  }
+
+  toggleSynchronousApprovals(): void {
+    this.#useSyncApprovals = !this.#useSyncApprovals;
+    console.log(`[SNAP] Synchronous approvals: ${this.#useSyncApprovals}`);
+  }
+
+  isSynchronousMode(): boolean {
+    return this.#useSyncApprovals;
   }
 }
