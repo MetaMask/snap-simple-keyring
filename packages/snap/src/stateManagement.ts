373b48203a85c4c993f2bb332d81b950d3f6862c
Daniel Rocha <daniel.rocha@consensys.net>
2023-09-15 19:43:14 +0200

    fix: persist snap state after switching between sync <-> async (#76)

--- packages/snap/src/stateManagement.ts
+++ packages/snap/src/stateManagement.ts
@@ -1,23 +1,31 @@
 import type { KeyringState } from './keyring';
 
 /**
+ * Default keyring state.
+ */
+const defaultState: KeyringState = {
+  wallets: {},
+  pendingRequests: {},
+  useSyncApprovals: true,
+};
+
+/**
  * Retrieves the current state of the keyring.
  *
  * @returns The current state of the keyring.
  */
 export async function getState(): Promise<KeyringState> {
-  const state = await snap.request({
+  const state = (await snap.request({
     method: 'snap_manageState',
     params: { operation: 'get' },
-  });
+  })) as any;
 
   console.log('[Snap] get state', state);
 
   return {
-    wallets: {},
-    requests: {},
+    ...defaultState,
     ...state,
-  } as KeyringState;
+  };
 }
 
 /**
