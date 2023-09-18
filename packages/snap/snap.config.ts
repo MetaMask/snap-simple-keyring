cee11a31ffe0b4e8a968e721d065fb9f0ad7a0b4
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-09-18 11:30:11 +0200

    build: ignore unused builtins

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -6,7 +6,24 @@ const config: SnapConfig = {
   server: { port: 8080 },
   polyfills: {
     buffer: true,
+    stream: true,
     crypto: true,
+  },
+  stats: {
+    builtIns: {
+      // The following builtins can be ignored. They are used by some of the
+      // dependencies, but are not required by this snap.
+      ignore: [
+        'events',
+        'http',
+        'https',
+        'zlib',
+        'util',
+        'url',
+        'string_decoder',
+        'punycode',
+      ],
+    },
   },
 };
 
