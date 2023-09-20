0f5034002ec76ea9b6944623810ec4cac7089abf
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-09-20 04:48:36 -0300

    fix: remove private key from options after generating key pair (#90)
    * fix: remove private key from options after generating key pair
    
    * chore: update snap shasum
    
    * chore: document why the private key isn't stored in the account options
    
    ---------
    
    Co-authored-by: Daniel Rocha <68558152+danroc@users.noreply.github.com>

--- packages/snap/src/keyring.ts
+++ packages/snap/src/keyring.ts
@@ -81,6 +81,13 @@ export class SimpleKeyring implements Keyring {
       throw new Error(`Account address already in use: ${address}`);
     }
 
+    // The private key should not be stored in the account options since the
+    // account object is exposed to external components, such as MetaMask and
+    // the snap UI.
+    if (options?.privateKey) {
+      delete options.privateKey;
+    }
+
     const account: KeyringAccount = {
       id: uuid(),
       options,
