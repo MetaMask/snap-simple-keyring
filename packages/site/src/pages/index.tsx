41294a20938ae0a1d3a2687b12e47b817a263191
gantunesr <gustavoantunesrod@gmail.com>
2023-10-11 19:58:48 -0300

    fix: update text

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -309,7 +309,7 @@ const Index = () => {
     <Container>
       <AlertBanner
         title={
-          'This is a developer tool for testing purposes only. It should not be used to store any real funds. Use it with caution.'
+          'This is a developer tool for testing purposes only. It should not be used to store any real funds. Use with caution.'
         }
         alertType={AlertType.Failure}
       />
